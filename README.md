79a04cb7d489124415969197a816315af98dc1ea
Julien Barrois <barrois.julien@gmail.com>
2025-01-21 12:31:04 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # maybe-chromium-bug
-repro case of bug
+repro case of bug happening when trying to access property of a big object created with JSON.parse from a .json file
+
+to repro :
+- yarn
+- yarn start
+- click on the button 'Click Me' and search for the file `big.json` located in public\
+
+you should get an exception on chrome, nothing on firefox
---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+
+class FileDialogOpener {
+  readonly fileInputRef: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>()
+  onFileOpen: (file: File) => void = null!
+  open(onFileOpen: (file: File) => void): void {
+    this.onFileOpen = onFileOpen
+    this.fileInputRef.current?.click()
+  }
+}
+
+const theFileDialogOpener = new FileDialogOpener()
+
+export const FileDialogOpenerComp =  ({ fileDialogOpener }: { fileDialogOpener: FileDialogOpener }) => {
+    const handleChange = React.useCallback(
+      (event: React.FormEvent<HTMLInputElement>): void => {
+        const target = event.currentTarget
+        if (target.files) {
+          const file = target.files.item(0)
+          if (file) {
+            event.preventDefault()
+            fileDialogOpener.onFileOpen?.(file)
+          }
+        }
+        target.value = ''
+      },
+      [fileDialogOpener]
+    )
+
+
+    return (
+        <dialog>
+          <input
+            type="file"
+            style={{ display: 'none' }}
+            ref={fileDialogOpener.fileInputRef}
+            accept={'.json'}
+            onChange={handleChange}
+          />
+        </dialog>
+    )
+  }
+
+
+
+function deepClone(input : any, path = ''):any {
+  if (input === null) {
+      return null
+  }
+  if (Array.isArray(input)) {
+      return input.map((v, i) => deepClone(v, `${path}[${i}]`))
+  }
+  if (typeof input === 'object') {
+      const keys = Object.keys(input)
+      if (keys.length > 0) {
+        const firstKey = keys[0]
+        if (!(firstKey in input)) {
+          throw new Error(`${keys[0]} not in Object ${JSON.stringify(input).substring(0, 50)}...`)
+        }
+      }
+
+      const output:Record<string, any> = {}
+      for (const key in input) {
+          const inputValue = input[key]
+          if (inputValue === undefined) {
+              throw new Error(`undefined value for key ${key}`)
+          }
+          output[key] = deepClone(inputValue, `${path}[${key}]`)
+      }
+      return input
+  }
+
+  return input
+}
+
+
+function click() {
+  theFileDialogOpener.open((file: File) => {
+    const fileReader = new FileReader() // FileReader is browser only
+    fileReader.onload = (): void => {
+      if (fileReader.result && typeof fileReader.result === 'string') {
+        const data = JSON.parse(fileReader.result)
+        const deepClones: any[] = []
+        for (let i = 0; i < 1000; i++) {
+            console.log(`deepClone ${i}`)
+            deepClones.push(deepClone(data))
+
+        }
+
+        console.log('done')
+      }
+    }
+
+    fileReader.readAsText(file, 'utf8')
+  })
+}
+
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <FileDialogOpenerComp fileDialogOpener={theFileDialogOpener}/>
+        <p>
+          <button onClick={() => click()}>Click Me</button>
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
